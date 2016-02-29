725b54e1644be8f49ddb9346baa508bd6288ad36
Omar Ahmed Fouad Hassan Wasfy. <eng.omarwasfy@gmail.com>
2016-02-29 21:57:56 +0000

    Main.java edited online with Bitbucket 6

--- hangMan/src/eg/edu/alexu/csd/datastructure/hangman/cs46/Main.java
+++ hangMan/src/eg/edu/alexu/csd/datastructure/hangman/cs46/Main.java
@@ -1,50 +1,34 @@
 package eg.edu.alexu.csd.datastructure.hangman.cs46;
 
-import java.util.Random;
+
+
 import java.util.Scanner;
 
-import javax.swing.plaf.synth.SynthSeparatorUI;
+import eg.edu.alexu.csd.datastructure.hangman.IHangman;
 
 public class Main {
 
-	static int x;
-	static String show;
-	static char c;
-	static Integer max = new Integer(0);
-	static Character ch = new Character('a');
-	static String secretWord;
-	static String[] words = new String[5];
-
+	static String[] directionary = new String[] { "BELGIUM", "BURUNDI", "COLOMBIA", "EGYPT", "KAZAKHSTAN", "MAURITANIA", "SINGAPORE", "UZBEKISTAN" }; // this should be read from file, instead of having them hard-coded
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		MyHangMan tester = new MyHangMan();
-		Scanner sc = new Scanner(System.in);
-		for (int i = 0; i <= 4; i++) {
-			System.out.println("please enter word " + (i+1));
-			words[i] = sc.next();
-		}
-		tester.setDictionary(words);
-		tester.selectRandomSecretWord();
-		secretWord = tester.word;
-		System.out.println("please enter maxnumber of mistakes");
-		max= sc.nextInt();
-		tester.setMaxWrongGuesses(max);
-		while (true) {
-			System.out.println("please enter char");
-			c = sc.next().charAt(0);
-			ch=c;
-			show = tester.guess(ch);
-			if (show == null) {
-				System.out.println("Lose");
-				break;
-			} else if (!show.contains("-")) {
-				System.out.println("Win");
-				System.out.println(show);
-				break;
+		MyHangMan hangman = new MyHangMan() ;   // Here you will create an object of your class
+		hangman.setDictionary(directionary);
+		hangman.setMaxWrongGuesses(5);
+		String secret = hangman.selectRandomSecretWord();
+		Scanner input = new Scanner(System.in);  // Get user input
+		Character guess = null;
+		do{
+			String result = hangman.guess(guess);
+			if(result==null){
+				System.out.println("Fail! correct answer = '" + secret + "'"); // fail
+				return;
 			}
-			System.out.println(show);
-
-		}
-		sc.close();
+			System.out.println(result);
+			if(!result.contains("-")){
+				System.out.println("Well Done!");  // win
+				return;
+			}
+			guess = input.next().toUpperCase().charAt(0);
+		}while(true);
 	}
 }
