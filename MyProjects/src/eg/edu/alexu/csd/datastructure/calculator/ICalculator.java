a1b14ed748b6e67f86c41f8f4e3cd782fb654fa0
omarwasfy <eng.omarwasfy@gmail.com>
2016-03-13 01:57:30 +0200

    new start again

---
+++ MyProjects/src/eg/edu/alexu/csd/datastructure/calculator/ICalculator.java
@@ -0,0 +1,18 @@
+package eg.edu.alexu.csd.datastructure.calculator;
+
+public interface ICalculator {
+	/**
+	* Adds given two numbers
+	* @param x first number
+	* @param y second number
+	* @return the sum of the two numbers
+	*/
+	int add(int x, int y);
+	/**
+	* Divids two numbers
+	* @param x first number
+	* @param y second number
+	* @return the division result
+	*/
+	float divide(int x, int y);
+}
