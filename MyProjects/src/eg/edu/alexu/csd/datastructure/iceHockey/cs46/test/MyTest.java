f457ccf80167f6643e41dad5f9ddaa418e690916
omarwasfy <eng.omarwasfy@gmail.com>
2016-03-20 01:55:21 +0200

    IceHockey with test first try

--- MyProjects/src/eg/edu/alexu/csd/datastructure/iceHockey/cs46/test/MyTest.java
+++ MyProjects/src/eg/edu/alexu/csd/datastructure/iceHockey/cs46/test/MyTest.java
@@ -2,13 +2,34 @@ package eg.edu.alexu.csd.datastructure.iceHockey.cs46.test;
 
 import static org.junit.Assert.*;
 
+import java.awt.Point;
+
 import org.junit.Test;
+
+import eg.edu.alexu.csd.datastructure.iceHockey.cs46.MyIceHockey;
 
 public class MyTest {
 
 	@Test
 	public void test() {
-		fail("Not yet implemented");
+		MyIceHockey test = new MyIceHockey();
+		String[] image = {
+				"33JUBU33", 
+				"3U3O4433", 
+				"O33P44NB", 
+				"PO3NSDP3", 
+				"VNDSD333", 
+				"OINFD33X"};
+		Point[] answer = 
+			new Point[]{
+				new Point(4, 5),
+				new Point(13, 9),
+				new Point(14, 2),
+			};
+		Integer team = 3;
+		Integer threashold = 16;
+		Point[] result =test.findPlayers(image, team, threashold);
+		assertArrayEquals(answer, result);
 	}
 
 }
