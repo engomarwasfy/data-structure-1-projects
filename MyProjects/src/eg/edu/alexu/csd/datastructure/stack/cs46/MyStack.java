ce87386773000b709d4db24cbd98ea358749f9e7
omarwasfy <eng.omarwasfy@gmail.com>
2016-04-17 22:37:02 +0200

    try

--- MyProjects/src/eg/edu/alexu/csd/datastructure/stack/cs46/MyStack.java
+++ MyProjects/src/eg/edu/alexu/csd/datastructure/stack/cs46/MyStack.java
@@ -1,92 +1,44 @@
 package eg.edu.alexu.csd.datastructure.stack.cs46;
-
 import eg.edu.alexu.csd.datastructure.linkedList.cs46.OurSingleLinked;
 import eg.edu.alexu.csd.datastructure.stack.IStack;
-
 public class MyStack implements IStack {
-
-    private int size = 0;
-    private Object[] Stack;
-
+    OurSingleLinked help;
     public MyStack() {
-       Stack = new Object[999999];
-       size=0;
+        help=new OurSingleLinked();
     }
-
     @Override
     public void add(final int index, final Object element) {
         // TODO Auto-generated method stub
-        OurSingleLinked helplinked=new OurSingleLinked();
-        for(int i=0;i<size;i++){
-            helplinked.add(Stack[i]);
-        }
-        helplinked.add(index, element);
-        for(int i=0;i<size;i++){
-            Stack[i]=null;
-        }
-        for(int i=0;i<helplinked.size();i++){
-            Stack[i]=helplinked.get(i);
-        }
-        size++;
-    
+    help.add(index, element);        
     }
     @Override
     public Object pop() {
         // TODO Auto-generated method stub
-        Object e;
-        if (size == 0) {
-            throw new RuntimeException("Check your inputs from pop");
-        } else {
-            e = Stack[size];
-            Stack[size] = null;
-            size--;
-            return e;
-        }
+       Object e = help.get(help.size()-1);
+       help.remove(help.size()-1);
+       return e;
     }
-
     @Override
     public Object peek() {
         // TODO Auto-generated method stub
-        if (size == 0) {
-            throw new RuntimeException("Check your inputs from peek");
-        } else {
-            return Stack[size];
-        }
+       return  help.get(help.size()-1);
     }
-
     @Override
     public void push(final Object element) {
         // TODO Auto-generated method stub
-        size++;
-        Stack[size] = element;
+     help.add(element); 
     }
-
     @Override
     public boolean isEmpty() {
         // TODO Auto-generated method stub
-        return size == 0;
+       return help.isEmpty();
     }
-
     @Override
     public int size() {
         // TODO Auto-generated method stub
-        return size;
+        return help.size();
     }
 }
 
-/*    if (index >=0  && index <= size + 1) {
-MyStack help = new MyStack();
-for (int i = size; i >= index; i--) {
-    help.push(this.pop());
-}
-this.push(element);
-while (help.size != 0) {
-    this.push(help.pop());
-}
-}
-else {
-throw new RuntimeException("Check your inputs from add");
-}
-}*/
 
 
