5f2eae0535e9dcb8fca6622cbbb9834ef2b6a0fb
omarwasfy <eng.omarwasfy@gmail.com>
2016-05-18 01:06:45 +0200

    try

--- MyProjects/src/eg/edu/alexu/csd/datastructure/queue/cs46/MyQueueArrayBasedTest.java
+++ MyProjects/src/eg/edu/alexu/csd/datastructure/queue/cs46/MyQueueArrayBasedTest.java
@@ -7,56 +7,90 @@ import org.junit.Test;
  * @author wasfy this
  *
  */
+
 public class MyQueueArrayBasedTest {
-    int n = 5;
+    /**
+     * @author wasfy this
+     *
+     */
+
+    private static final int m5 = 5;
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testIsEmpty1() {
-        MyQueueArray queue = new MyQueueArray(n);
+        MyQueueArray queue = new MyQueueArray(m5);
         assertTrue(queue.isEmpty());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testIsEmpty2() {
-        MyQueueArray queue = new MyQueueArray(n);
-        queue.enqueue(5);
+        MyQueueArray queue = new MyQueueArray(m5);
+        queue.enqueue(m5);
         assertTrue(!queue.isEmpty());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testEnqueue() {
-        MyQueueArray queue = new MyQueueArray(n);
-        queue.enqueue(5);
+        MyQueueArray queue = new MyQueueArray(m5);
+        queue.enqueue(m5);
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testDequeue() {
-        MyQueueArray queue = new MyQueueArray(n);
-        queue.enqueue(5);
-        assertTrue(5 == (int) queue.dequeue());
+        MyQueueArray queue = new MyQueueArray(m5);
+        queue.enqueue(m5);
+        assertTrue(m5 == (int) queue.dequeue());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testGetRightOrder() {
-        MyQueueArray queue = new MyQueueArray(n);
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        assertEquals(1, queue.dequeue());
-        assertEquals(2, queue.dequeue());
-        assertEquals(3, queue.dequeue());
+        MyQueueArray queue = new MyQueueArray(m5);
+        queue.enqueue(m5);
+        queue.enqueue(m5);
+        queue.enqueue(m5);
+        assertEquals(m5, queue.dequeue());
+        assertEquals(m5, queue.dequeue());
+        assertEquals(m5, queue.dequeue());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test(expected = RuntimeException.class)
     public void testDequeueEmpty() {
-        MyQueueArray queue = new MyQueueArray(n);
+        MyQueueArray queue = new MyQueueArray(m5);
         queue.dequeue();
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testEmpty() {
-        MyQueueArray queue = new MyQueueArray(n);
-        queue.enqueue(5);
+        MyQueueArray queue = new MyQueueArray(m5);
+        queue.enqueue(m5);
         queue.dequeue();
         assertTrue(queue.isEmpty());
     }
--- MyProjects/src/eg/edu/alexu/csd/datastructure/queue/cs46/MyQueueLinkedListTest.java
+++ MyProjects/src/eg/edu/alexu/csd/datastructure/queue/cs46/MyQueueLinkedListTest.java
@@ -3,56 +3,93 @@ package eg.edu.alexu.csd.datastructure.queue.cs46;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
+/**
+ * @author wasfy this
+ *
+ */
 
 public class MyQueueLinkedListTest {
-
+    /**
+     * @author wasfy this
+     *
+     */
+    private static final int m5 = 5;
+    /**
+     * @author wasfy this
+     *
+     */
     @Test
     public void testIsEmpty1() {
         MyQueueLinked queue = new MyQueueLinked();
         assertTrue(queue.isEmpty());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testIsEmpty2() {
         MyQueueLinked queue = new MyQueueLinked();
-        queue.enqueue(5);
+        queue.enqueue(m5);
         assertTrue(!queue.isEmpty());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testEnqueue() {
         MyQueueLinked queue = new MyQueueLinked();
-        queue.enqueue(5);
+        queue.enqueue(m5);
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testDequeue() {
         MyQueueLinked queue = new MyQueueLinked();
-        queue.enqueue(5);
-        assertTrue(5 == (int) queue.dequeue());
+        queue.enqueue(m5);
+        assertTrue(m5 == (int) queue.dequeue());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test
     public void testGetRightOrder() {
         MyQueueLinked queue = new MyQueueLinked();
-        queue.enqueue(1);
-        queue.enqueue(2);
-        queue.enqueue(3);
-        assertEquals(1, queue.dequeue());
-        assertEquals(2, queue.dequeue());
-        assertEquals(3, queue.dequeue());
+        queue.enqueue(m5);
+        queue.enqueue(m5);
+        queue.enqueue(m5);
+        assertEquals(m5, queue.dequeue());
+        assertEquals(m5, queue.dequeue());
+        assertEquals(m5, queue.dequeue());
     }
+    /**
+     * @author wasfy this
+     *
+     */
 
     @Test(expected = RuntimeException.class)
     public void testDequeueEmpty() {
         MyQueueLinked queue = new MyQueueLinked();
         queue.dequeue();
     }
+    /**
+     * @author wasfy this
+     *
+     */
+
 
     @Test
     public void testEmpty() {
         MyQueueLinked queue = new MyQueueLinked();
-        queue.enqueue(5);
+        queue.enqueue(m5);
         queue.dequeue();
         assertTrue(queue.isEmpty());
     }
