4f904d3942dffb9155acef698b608056f580c9b1
omarwasfy <eng.omarwasfy@gmail.com>
2016-05-18 02:45:16 +0200

    try

--- MyProjects/src/eg/edu/alexu/csd/datastructure/linkedList/cs46/LinkedListTest.java
+++ MyProjects/src/eg/edu/alexu/csd/datastructure/linkedList/cs46/LinkedListTest.java
@@ -1,8 +1,6 @@
 package eg.edu.alexu.csd.datastructure.linkedList.cs46;
-import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertTrue;
+
 
 import org.junit.Test;
 
